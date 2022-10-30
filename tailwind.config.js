458daa89430b300bc2d17dfaca2c4a2e4bea8d44
Safwa9amar <hassanih97@gmail.com>
2022-10-30 10:54:36 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html", "**/*.js"],
+  theme: {
+    extend: {},
+    // screens: {
+    //   md: "375px",
+    //   // => @media (min-width: 375px) { ... }
+
+    //   desktop: "1440px",
+    //   // => @media (min-width: 1440px) { ... }
+    // },
+    // colors: {
+    //   // Configure your color palette here
+    //   'white': "hsl(0, 0%, 98%)",
+    //   'gray': "hsl(0, 0%, 41%)",
+    //   'dark': "hsl(0, 0%, 8%)",
+    // },
+  },
+  plugins: [],
+};
